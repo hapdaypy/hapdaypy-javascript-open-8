7aea040e00a35ef58032cbae4a7d75bc4e6de457
unknown <alsdnrskfk85@naver.com>
2025-11-11 19:35:09 +0900

    feat: 사람들을 관리하는 기능 생성, 출석 여부 확인 기능 생성

---
+++ open_4/src/model/Player.js
@@ -0,0 +1,38 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class player {
+  #name;
+  #horseName;
+  #isPresent = false;
+  #record;
+
+  constructor(name, horseName) {
+    this.#name = name;
+    this.#horseName = horseName;
+    this.#isPresent = false;
+    this.#record = "";
+  }
+
+  checkIn() {
+    this.#isPresent = true;
+    MissionUtils.Console.print(`${this.#name} 출석 완료`);
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get horseName() {
+    return this.#horseName;
+  }
+
+  getInfo() {
+    return {
+      name: this.#name,
+      horse: this.#horseName,
+      present: this.#isPresent,
+    };
+  }
+}
+
+export default player;

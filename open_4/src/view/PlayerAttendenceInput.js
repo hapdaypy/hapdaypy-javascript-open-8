62f4ae323bbea4df15153c6daf7025501ac8470d
unknown <alsdnrskfk85@naver.com>
2025-11-11 23:29:51 +0900

    feat: 객체지향을 사용하여 플레이어를 입력 받고 플레이러의 출석여부를 관리하는 기능 추가

---
+++ open_4/src/view/PlayerAttendenceInput.js
@@ -0,0 +1,41 @@
+import { Console } from "@woowacourse/mission-utils";
+import PlayerManager from "../model/PlayerManager.js";
+
+class PlayerAndHourseInput {
+  constructor() {
+    this.playerAttendenceList = new PlayerManager();
+  }
+  async collecPlayers() {
+    // 사용자 사전 입력
+
+    for (let index = 0; index < 2; index++) {
+      const playerAndHorse = await Console.readLineAsync(
+        "선수와 말을 입력해주세요.\n"
+      );
+
+      const [player, horse] = playerAndHorse
+        .split(",")
+        .map((item) => item.trim());
+
+      this.playerAttendenceList.addNewPlayer(player, horse);
+    }
+  }
+
+  async AttendenceCheckPlayers() {
+    for (let index = 0; index < 2; index++) {
+      const attendencePlayerAndHorse = await Console.readLineAsync(
+        "경기장에 도착한 선수와 말을 입력해주세요.\n"
+      );
+      const [player, horse] = attendencePlayerAndHorse
+        .split(",")
+        .map((item) => item.trim());
+      this.playerAttendenceList.makeAttendence(player, horse);
+    }
+  }
+
+  print() {
+    Console.print(this.playerAttendenceList.getAllPlayerInfo());
+  }
+}
+
+export default PlayerAndHourseInput;

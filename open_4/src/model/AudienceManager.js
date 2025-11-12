52a3895daa6ec995ffcb68a04a284e95837c36f9
unknown <alsdnrskfk85@naver.com>
2025-11-12 21:27:41 +0900

    feat: 관객의 수를 알 수 있는 기능 추가

--- open_4/src/model/AudienceManager.js
+++ open_4/src/model/AudienceManager.js
@@ -15,6 +15,10 @@ class AudienceManager {
   getAllAudienceInfo() {
     return this.#audiences.map((p) => p.getInfo());
   }
+
+  getAudienceListSize() {
+    return this.#audiences.length;
+  }
 }
 
 export default AudienceManager;

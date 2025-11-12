b0a3f010d365af66f7865e0e34b0aa8fc6c605a8
unknown <alsdnrskfk85@naver.com>
2025-11-12 20:41:47 +0900

    feat: omr 클래스를 만들어서 사용자가 저장해야할 항목들을 한번에 관리하는 기능 생성 및 요소들을 확인해 볼 수 있는get 기능 추가

---
+++ open_4/src/model/Omr.js
@@ -0,0 +1,39 @@
+import { MissionUtils } from "@woowacourse/mission-utils";
+
+class Omr {
+  // OMR 을 관리하는 클래스
+
+  #methodOfWinning; // 승식
+  #betAmount;
+  #selectHourse;
+  #accidentStatus = false;
+
+  constructor(methodOfWinning, betAmount, selectHourse) {
+    this.#methodOfWinning = methodOfWinning;
+    this.#betAmount = Number(betAmount);
+    this.#selectHourse = selectHourse;
+  }
+
+  getInfoOmr() {
+    return {
+      methodOfWinning: this.#methodOfWinning,
+      betAmount: this.#betAmount,
+      selectHourse: this.#selectHourse,
+    };
+  }
+  getMethodOfWinning() {
+    return this.#methodOfWinning;
+  }
+
+  getBetAmount() {
+    return this.this.#betAmount;
+  }
+
+  getselectHourse() {
+    return this.#selectHourse;
+  }
+
+  // 형식이 다 맞게 되었는지 검사해야함
+}
+
+export default Omr;

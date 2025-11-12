1f5d62e850eb124cc2ee659e647be5e24a2489e5
unknown <alsdnrskfk85@naver.com>
2025-11-12 20:45:17 +0900

    feat: 돈을 관리하는 클래스인데 static을 이용해서 new 없이도 사용할 수 있게 설정

--- open_4/src/model/MoneyManagement.js
+++ open_4/src/model/MoneyManagement.js
@@ -1,10 +1,14 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
-class moneyManager {
-  checkEntranceFee(entranceFee) {
-    if (entranceFee === 2000) return true;
-    else if (entranceFee !== 2000) return false;
+class MoneyManager {
+  static checkEntranceFee(entranceFee) {
+    const entranceFeeNumber = Number(entranceFee);
+    if (entranceFeeNumber >= 2000) return true;
+    else if (entranceFeeNumber < 2000) return false;
   }
 }
-
-export default moneyManager;
+/*
+예외케이스
+1. 
+*/
+export default MoneyManager;

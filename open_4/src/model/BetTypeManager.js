064ef5feb9670c054f2ea08a9bcee38a2dd3e09d
unknown <alsdnrskfk85@naver.com>
2025-11-13 18:36:49 +0900

    feat: 승식별로 배팅된 금액 출력하는 클래스 생성

---
+++ open_4/src/model/BetTypeManager.js
@@ -0,0 +1,34 @@
+class BetTypeManager {
+  #totals;
+
+  constructor() {
+    this.#totals = {
+      win: 0, // 단승
+      place: 0, // 연승
+      quinella: 0, // 복승
+      exacta: 0, // 쌍승
+    };
+  }
+
+  addBet(koreanType, amount) {
+    const typeKey = this.translateBetType(koreanType);
+    if (typeKey in this.#totals) {
+      this.#totals[typeKey] += amount;
+    }
+  }
+
+  translateBetType(koreanType) {
+    const typeMap = {
+      단승: "win",
+      연승: "place",
+      복승: "quinella",
+      쌍승: "exacta",
+    };
+    return typeMap[koreanType];
+  }
+  getTotal() {
+    return { ...this.#totals };
+  }
+}
+
+export default BetTypeManager;

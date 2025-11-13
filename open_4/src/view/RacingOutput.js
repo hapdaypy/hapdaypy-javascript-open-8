36efaa0e846656d2f501f0f65f396174f4c9eb8a
unknown <alsdnrskfk85@naver.com>
2025-11-13 21:53:27 +0900

    feat: 경기와 관련된 부분을 출력하는 매서드 생성

---
+++ open_4/src/view/RacingOutput.js
@@ -0,0 +1,16 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class RacingOutput {
+  static printRacingView(playerArray) {
+    for (const player of playerArray) {
+      Console.print(`${player.getName()} : ${player.getRecord()}`);
+    }
+    Console.print("");
+  }
+  static printRank(rankArray) {
+    rankArray.forEach((player, index) => {
+      Console.print(`${index + 1}등: ${player.getName()}`);
+    });
+  }
+}
+export default RacingOutput;

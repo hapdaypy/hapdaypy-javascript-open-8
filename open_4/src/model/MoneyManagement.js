75888e0786fef59434b4b628af4e34d44698ffea
unknown <alsdnrskfk85@naver.com>
2025-11-13 18:35:50 +0900

    feat: 승식별로 베팅된 금액을 계산하는 코드 생성

--- open_4/src/model/MoneyManagement.js
+++ open_4/src/model/MoneyManagement.js
@@ -6,7 +6,21 @@ class MoneyManager {
     if (entranceFeeNumber >= 2000) return true;
     else if (entranceFeeNumber < 2000) return false;
   }
+
+  calculateTotalBets(audienceInput, betType) {
+    const audienceManager = audienceInput.getAudienceListReturn();
+    const audienceArray = audienceManager.getAudience(); // 이용자 배열 생성
+
+    for (let index = 0; index < audienceArray.length; index++) {
+      betType.addBet(
+        // OMR 에 저장되어 있는 승식과, 베팅금액이 나오게 됨
+        audienceArray[index].getBettingType(),
+        audienceArray[index].getBettingMoney()
+      );
+    }
+  }
 }
+
 /*
 예외케이스
 1. 

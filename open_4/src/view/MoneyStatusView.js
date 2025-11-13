255ac6ec70b246b863cb50651515e3762fc7e07d
unknown <alsdnrskfk85@naver.com>
2025-11-13 18:37:29 +0900

    feat: 승식별로 배팅된 금액 출력하는 클래스 생성

---
+++ open_4/src/view/MoneyStatusView.js
@@ -0,0 +1,9 @@
+import { Console } from "@woowacourse/mission-utils";
+
+class MoneyStatusView {
+  static printBettingTotals(betTypeData) {
+    Console.print(betTypeData);
+  }
+}
+
+export default MoneyStatusView;

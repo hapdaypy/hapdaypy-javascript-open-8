7a479c83486469a8fd6cdb7ee0361caec910ca8b
unknown <alsdnrskfk85@naver.com>
2025-11-13 13:51:47 +0900

    chore: 이해를 돕는 주석 추가

--- open_4/src/view/PlayerAttendenceInput.js
+++ open_4/src/view/PlayerAttendenceInput.js
@@ -3,12 +3,11 @@ import PlayerManager from "../model/PlayerManager.js";
 
 class PlayerAndHourseInput {
   constructor() {
-    this.playerAttendenceList = new PlayerManager();
+    this.playerAttendenceList = new PlayerManager(); // 호출해서 실질적으로 배열을 만들게 됨-> 배열 생성
+    // playerManager 를 호출함으로써 배열로 관리하는 클래스 호출 후 -> 하나의 배열을 생성하게 됨
   }
-
   async collecPlayers() {
     // 사용자 사전 입력
-
     for (let index = 0; index < 2; index++) {
       const playerAndHorse = await Console.readLineAsync(
         "선수와 말을 입력해주세요.\n"
@@ -35,6 +34,10 @@ class PlayerAndHourseInput {
 
   print() {
     Console.print(this.playerAttendenceList.getAllPlayerInfo());
+  }
+
+  getPlayerAttendenceListReturn() {
+    return this.playerAttendenceList;
   }
 }
 

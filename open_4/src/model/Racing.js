f9785f940c9d0d8220e102218996dbf200240a05
unknown <alsdnrskfk85@naver.com>
2025-11-13 21:55:09 +0900

    feat: MVC 패턴을 지키기 위해서 outview를 호출하게 함

--- open_4/src/model/Racing.js
+++ open_4/src/model/Racing.js
@@ -1,31 +1,67 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
 
 class RacingStart {
+  #racingRanking;
   static TWENTY_FIVE = 25;
+  constructor() {
+    this.#racingRanking = [];
+  }
 
-  async run(playerInput) {
+  async run(playerInput, outputView) {
     const playerManager = playerInput.getPlayerAttendenceListReturn();
     const playerArray = playerManager.getPlayers();
     const playerNumber = playerArray.length; //  배열의 길이
 
-    for (let index = 0; index < 8; index++) {
+    while (this.#racingRanking.length !== playerArray.length) {
       this.RaceController(playerArray, playerNumber);
+      outputView.printRacingView(playerArray);
+    }
+
+    return this.#racingRanking;
+  }
+
+  RaceController(playerArray, playerNumber) {
+    for (let index = 0; index < playerNumber; index++) {
+      const referenceValue = this.makeRandomValue();
+
+      const validateStop = this.validateDistanceRecord(
+        playerArray[index].getRecord()
+      );
+
+      if (!validateStop && referenceValue >= RacingStart.TWENTY_FIVE) {
+        playerArray[index].addRecord(); // player라는 클래스에 접근함
+        this.determineRanks(playerArray[index].getRecord(), playerArray[index]);
+      }
     }
   }
 
   makeRandomValue() {
     return MissionUtils.Random.pickNumberInRange(1, 45);
   }
+  validateDistanceRecord(record) {
+    if (record.length === 5) return true;
+    return false;
+  }
 
-  RaceController(playerArray, playerNumber) {
-    for (let index = 0; index < playerNumber; index++) {
-      const referenceValue = this.makeRandomValue();
-      if (referenceValue >= RacingStart.TWENTY_FIVE) {
-        // static 사용법
-
-        playerArray[index].addRecord(); // player라는 클래스에 접근함
-      }
+  determineRanks(record, player) {
+    const isAlreadyRanked = this.#racingRanking.includes(player);
+    if ((record.length === 5) != isAlreadyRanked) {
+      this.#racingRanking.push(player);
     }
   }
 }
+
 export default RacingStart;
+
+/*
+
+먼저 5칸을 들어오는 말이 이기는 거임
+
+말을 전진시킬 때마다 출력을 해야함 
+
+도착한 말은 더이상 recording 되지 않음
+----- 가 5개가 나와야함 
+
+
+
+*/

16a0d2362a80afaa1df2d9642b1b25c5ab467398
unknown <alsdnrskfk85@naver.com>
2025-11-13 18:36:18 +0900

    chore: 공백 수정

--- open_4/src/view/AudienceInformationInput.js
+++ open_4/src/view/AudienceInformationInput.js
@@ -12,7 +12,6 @@ class PlayerAndHourseInput {
     for (let index = 0; index < 2; index++) {
       const entranceFee = await Console.readLineAsync("입장료를 내주세요.\n");
       const entryResult = MoneyManager.checkEntranceFee(entranceFee);
-
       if (entryResult === true) {
         const audienceNamn = await Console.readLineAsync(
           "관객의 이름을 입력해주세요.\n"
@@ -20,11 +19,9 @@ class PlayerAndHourseInput {
         const audienceOmrString = await Console.readLineAsync(
           "OMR을 작성해주세요. 승식/베팅금/선택한 말 순으로 기입해주세요\n"
         ); // Omr은 승식, 베팅금, 선택한 말 순서로 입력 받음
-
         const omrSplit = audienceOmrString.split("/");
         const [method, bettingAmount, selectHourse] = omrSplit;
         const audienceOmrClass = new Omr(method, bettingAmount, selectHourse);
-
         this.audienceList.addNewAudience(audienceNamn, audienceOmrClass);
       }
     }
@@ -32,6 +29,10 @@ class PlayerAndHourseInput {
 
   print() {
     Console.print(this.audienceList.getAllAudienceInfo());
+  }
+
+  getAudienceListReturn() {
+    return this.audienceList;
   }
 }
 

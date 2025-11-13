40f03ec7127a07ac9768c85d474985e80d54a7d6
unknown <alsdnrskfk85@naver.com>
2025-11-13 18:37:55 +0900

    feat: 승식별로 배팅된 금액을 계산하는 클래스를 호출하는 코드 작성

--- open_4/src/App.js
+++ open_4/src/App.js
@@ -1,7 +1,9 @@
-import { Console } from "@woowacourse/mission-utils";
 import PlayerAttendenceInput from "./view/PlayerAttendenceInput.js";
 import AudienceInformationInput from "./view/AudienceInformationInput.js";
 import RacingStart from "./model/Racing.js";
+import BetTypeManager from "./model/BetTypeManager.js";
+import MoneyManager from "./model/MoneyManagement.js";
+import MoneyStatusView from "./view/MoneyStatusView.js";
 class App {
   async run() {
     // 선수들을 입장시키고 출석 결과 확인하기
@@ -15,21 +17,21 @@ class App {
     await audienceInput.collecAudience();
     audienceInput.print();
 
-    // 경기를 진행 시킴
-    // const accidentVictim = accidentOccurred();
+    //승 식 별 로 베 팅 금 액 을 계산
+    const betType = new BetTypeManager();
+    const moneymanager = new MoneyManager();
+    moneymanager.calculateTotalBets(audienceInput, betType);
+    MoneyStatusView.printBettingTotals(betType.getTotal());
+
+    //경 기 를 진 행 시 킴
     const race = new RacingStart();
     await race.run(playerInput); //
     playerInput.print();
-
-    /*
-    run 이라는 매세더를 호출하게 됨
-    */
+    /*ui 기능을 분리하기 위한 나의 노력*/
 
     // 경기 종료 후 정산
-    // adjustmentMoney(accidentVictim);
 
     // 최종 결과 진행
-    // resultPublished();
   }
 }
 

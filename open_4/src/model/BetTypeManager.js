eb72e2846c3d30f0bf3aff3a656d4d0c87d466a3
unknown <alsdnrskfk85@naver.com>
2025-11-14 15:42:24 +0900

    feat: 매세드 추가, 객체 리터럴 생성

--- open_4/src/model/BetTypeManager.js
+++ open_4/src/model/BetTypeManager.js
@@ -1,8 +1,23 @@
 class BetTypeManager {
   #totals;
-
+  #winningBetPool;
+  #payoutRate;
   constructor() {
     this.#totals = {
+      win: 0, // 단승
+      place: 0, // 연승
+      quinella: 0, // 복승
+      exacta: 0, // 쌍승
+    };
+
+    this.#winningBetPool = {
+      win: 0, // 단승
+      place: 0, // 연승
+      quinella: 0, // 복승
+      exacta: 0, // 쌍승
+    };
+
+    this.#payoutRate = {
       win: 0, // 단승
       place: 0, // 연승
       quinella: 0, // 복승
@@ -11,10 +26,22 @@ class BetTypeManager {
   }
 
   addBet(koreanType, amount) {
+    // 베팅금
     const typeKey = this.translateBetType(koreanType);
     if (typeKey in this.#totals) {
       this.#totals[typeKey] += amount;
     }
+  }
+
+  addWinnigPool(typeKey, amount) {
+    // 승리풀
+    if (typeKey in this.#winningBetPool) {
+      this.#winningBetPool[typeKey] += amount;
+    }
+  }
+
+  addPayoutRat(typeKey, winningPool) {
+    this.#payoutRate[typeKey] += winningPool;
   }
 
   translateBetType(koreanType) {
@@ -29,6 +56,18 @@ class BetTypeManager {
 
   getTotal() {
     return { ...this.#totals };
+  }
+  getTotalElement(key) {
+    return this.#totals[key];
+  }
+  getWinning(key) {
+    return this.#winningBetPool[key];
+  }
+  setPayout(key, payoutRate) {
+    return (this.#payoutRate[key] = payoutRate);
+  }
+  getPayoutRate(key) {
+    return this.#payoutRate[key];
   }
 }
 

f875d8653db7f4c6b20b28f8d4e54c9779f84c6f
unknown <alsdnrskfk85@naver.com>
2025-11-14 15:49:03 +0900

    feat: 배당률, 배당률에 따른 이득을 계산하는 매세드 생성

--- open_4/src/model/MoneyManagement.js
+++ open_4/src/model/MoneyManagement.js
@@ -1,4 +1,5 @@
-import { MissionUtils } from "@woowacourse/mission-utils";
+import { MissionUtils } from '@woowacourse/mission-utils';
+import WinningCalculator from './IsWinningBetCalculation.js';
 
 class MoneyManager {
   static checkEntranceFee(entranceFee) {
@@ -15,12 +16,81 @@ class MoneyManager {
       betType.addBet(
         // OMR 에 저장되어 있는 승식과, 베팅금액이 나오게 됨
         audienceArray[index].getBettingType(),
-        audienceArray[index].getBettingMoney()
+        audienceArray[index].getBettingMoney(),
       );
     }
   }
-}
 
+  winningPoolCalculation(betType, audienceInput, finalRanking) {
+    const audienceManager = audienceInput.getAudienceListReturn();
+    const audienceArray = audienceManager.getAudience();
+
+    for (let index = 0; index < audienceArray.length; index++) {
+      const koreanType = audienceArray[index].getBettingType();
+      const englishType = this.translateBetType(koreanType);
+      const audienceHorse = audienceArray[index].getHorse().split(','); // 배열로 만듦
+
+      const isCorrect = WinningCalculator.isWinning(
+        englishType,
+        audienceHorse,
+        finalRanking,
+      );
+      if (isCorrect === true) {
+        // 정답일 경우
+        audienceArray[index].setWinnig(true);
+
+        betType.addWinnigPool(
+          englishType,
+          audienceArray[index].getBettingMoney(),
+        );
+        // audience 맞췄으니까 맞춤여부에 체크표시
+      } else if (isCorrect === false) {
+        // 틀렸으니까 받을 돈 0 원리라고 해둬야지
+      }
+    }
+  }
+
+  payoutRatCalculation(betType) {
+    // 배당률을 구하는 코드
+    const betTypeKeys = ['win', 'place', 'quinella', 'exacta'];
+    for (const key of betTypeKeys) {
+      const total = betType.getTotalElement(key);
+      const winningPool = betType.getWinning(key);
+
+      let payoutRate = 0;
+      if (winningPool > 0) {
+        payoutRate = total / winningPool;
+      }
+      betType.setPayout(key, payoutRate);
+    }
+  }
+
+  translateBetType(koreanType) {
+    const typeMap = {
+      단승: 'win',
+      연승: 'place',
+      복승: 'quinella',
+      쌍승: 'exacta',
+      복연승: 'swinger',
+      삼복승: 'trio',
+    };
+    return typeMap[koreanType];
+  }
+
+  settleBets(betType, audienceInput) {
+    const audienceManager = audienceInput.getAudienceListReturn();
+    const audienceArray = audienceManager.getAudience(); // 사람들을 관리하는 배열 생성
+    for (const audience of audienceArray) {
+      if (audience.getWinning() === true) {
+        const koreanType = audience.getBettingType();
+        const englishType = this.translateBetType(koreanType);
+        const payoutRate = betType.getPayoutRate(englishType);
+        const money = payoutRate * audience.getBettingMoney();
+        audience.calculatePrizeMoney(money);
+      }
+    }
+  }
+}
 /*
 예외케이스
 1. 

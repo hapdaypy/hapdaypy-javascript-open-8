f74d72f71a16328dd15dc6bd5bcc78ba931426ed
unknown <alsdnrskfk85@naver.com>
2025-11-17 16:14:45 +0900

    feat: 잔돈 반환 안내 기능과 입장료 형식 검증 기능 추가

--- open_4/src/model/MoneyManagement.js
+++ open_4/src/model/MoneyManagement.js
@@ -1,11 +1,18 @@
-import { MissionUtils } from '@woowacourse/mission-utils';
+import { Console } from '@woowacourse/mission-utils';
 import WinningCalculator from './IsWinningBetCalculation.js';
 
 class MoneyManager {
   static checkEntranceFee(entranceFee) {
     const entranceFeeNumber = Number(entranceFee);
-    if (entranceFeeNumber >= 2000) return true;
-    else if (entranceFeeNumber < 2000) return false;
+    if (entranceFeeNumber >= 2000) {
+      const change = entranceFee - 2000;
+      Console.print(`잔돈은 ${change}원입니다.`);
+      return true;
+    } else if (entranceFeeNumber < 2000) {
+      Console.print(`입장료는 2000원입니다.`);
+
+      return false;
+    }
   }
 
   calculateTotalBets(audienceInput, betType) {

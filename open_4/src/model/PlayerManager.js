9267f6d0596c92cf3b67d654cb63d172ad360ff5
unknown <alsdnrskfk85@naver.com>
2025-11-18 16:15:03 +0900

    chore: 변수명 변경

--- open_4/src/model/PlayerManager.js
+++ open_4/src/model/PlayerManager.js
@@ -25,14 +25,15 @@ class PlayerManager {
   }
 
   makeAttendence(playerName, horseName) {
-    const areAllPlayersPresent = this.#players.find(
+    const isAnyoneAbsent = this.#players.some(
       (p) => p.getIsPresent() === false,
-    );
+    ); // 안 온 사람이 한명이라도 있을경우 ture
 
-    if (areAllPlayersPresent) {
+    if (isAnyoneAbsent) {
       let searchPlayer = this.#players.find(
         (p) => p.getName() === playerName && p.getHorseName() === horseName,
       );
+
       if (searchPlayer)
         searchPlayer.checkIn(); // 검사 통과해서 true 로 변하는 거 잖슴
       else {
@@ -40,10 +41,15 @@ class PlayerManager {
           `[ERROR] ${playerName} 선수(${horseName} 말)를 찾을 수 없거나, 정보가 일치하지 않아 출석에 실패했습니다.`,
         );
       }
-    } else {
+    }
+    const CurrentCheckAbsent = this.#players.some(
+      (p) => p.getIsPresent() === false,
+    ); // 안 온 사람이 한명이라도 있을경우 ture
+
+    if (!CurrentCheckAbsent) {
       Console.print(`모든 선수가 출석이 완료되었습니다.`);
       return true;
-    }
+    } else return false;
   }
   getAllPlayerInfo() {
     // 11월 16일 출석 여부에 따라서 출력해야함 ... 출석여부를 확인해야해

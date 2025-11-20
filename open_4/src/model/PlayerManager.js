2116be86a9d228c1cd8e02b2ecfaedc1cf5df22f
unknown <alsdnrskfk85@naver.com>
2025-11-20 16:39:36 +0900

    refactor: 불필요한 기능 삭제

--- open_4/src/model/PlayerManager.js
+++ open_4/src/model/PlayerManager.js
@@ -27,7 +27,6 @@ class PlayerManager {
     const isAnyoneAbsent = this.#players.some(
       (p) => p.getIsPresent() === false,
     ); // 안 온 사람이 한명이라도 있을경우 true
-
     if (isAnyoneAbsent) {
       let searchPlayer = this.#players.find(
         (p) => p.getName() === playerName && p.getHorseName() === horseName,
@@ -37,7 +36,7 @@ class PlayerManager {
         searchPlayer.checkIn(); // 검사 통과해서 true 로 변함
       else {
         Console.print(
-          `[ERROR] ${playerName} 선수(${horseName} 말)를 찾을 수 없거나, 정보가 일치하지 않아 출석에 실패했습니다.`,
+          `${playerName} 선수(${horseName} 말)를 찾을 수 없거나, 정보가 일치하지 않아 출석에 실패했습니다.`,
         );
       }
     }
@@ -52,6 +51,7 @@ class PlayerManager {
       return true;
     } else return false;
   }
+
   getAllPlayerInfo() {
     if (this.#players.length === 0) {
       return '현재 출석한 선수가 아무도 없습니다.';
